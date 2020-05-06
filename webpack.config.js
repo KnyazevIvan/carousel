fc70c7470c98a85c564b3854d366ef47cf0e2c0c
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-05-06 19:55:24 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin}= require('clean-webpack-plugin')
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/main.js',
   output: {
     filename: '[contenthash].js',
