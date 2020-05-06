771c5b4faa4d097a2954c694a5e2c6858a72c002
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-05-06 20:02:12 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 const {CleanWebpackPlugin}= require('clean-webpack-plugin')
+
 module.exports = {
   mode: 'production',
   entry: './src/main.js',
